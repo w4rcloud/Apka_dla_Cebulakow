ed0af31bf20ed939255bd3a561e64f1612303bbc
Mike <you@example.com>
2021-02-18 21:31:24 +0100

    Connections between Activities Background images

--- app/src/main/java/com/example/apkadlacebulakw/MainActivity.java
+++ app/src/main/java/com/example/apkadlacebulakw/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.apkadlacebulakw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToCalculator(View v) {
+        Intent i = new Intent(this, CalculatorActivity.class);
+        startActivity(i);
+    }
+
+    public void goToShoppingList(View v) {
+        Intent i = new Intent(this, ShoppingListActivity.class);
+        startActivity(i);
+    }
+
+    public void goToInfo(View v) {
+        Intent i = new Intent(this, InfoActivity.class);
+        startActivity(i);
     }
 }
