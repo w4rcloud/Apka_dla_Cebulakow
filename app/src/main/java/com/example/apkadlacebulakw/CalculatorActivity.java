d3b0df307e13ee01a5f067791f21bc30d839ffd7
Mike <you@example.com>
2021-02-19 01:17:52 +0100

    Calculator added, seems to be working Working on Dolan

--- app/src/main/java/com/example/apkadlacebulakw/CalculatorActivity.java
+++ app/src/main/java/com/example/apkadlacebulakw/CalculatorActivity.java
@@ -2,13 +2,46 @@ package com.example.apkadlacebulakw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
+    boolean dolanOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
     }
+
+    public void calculate(View v) {
+        double price = Double.parseDouble(((TextView) findViewById(R.id.price)).getText().toString());
+        double amount = Double.parseDouble(((TextView) findViewById(R.id.amount)).getText().toString());
+        if (amount == 0 && !dolanOn) {
+            findViewById(R.id.dolan).setVisibility(View.VISIBLE);
+        }
+        EditText t = findViewById(R.id.amount);
+        String input = t.getText().toString();
+        Log.d("result", ((TextView) findViewById(R.id.result)).getText().toString());
+
+//        double number = Double.parseDouble(price.gettex)
+
+        Log.d("price", String.valueOf(price));
+        Log.d("amount", String.valueOf(amount));
+        Log.d("price / amount", String.valueOf(price / amount));
+        Toast.makeText(this, String.valueOf(price / amount), Toast.LENGTH_LONG).show();
+        ((TextView) findViewById(R.id.result)).setText(String.valueOf(price / amount));
+    }
+
+    public void calculate2() {
+    }
+
 }
--- app/src/main/java/com/example/apkadlacebulakw/InfoActivity.java
+++ app/src/main/java/com/example/apkadlacebulakw/InfoActivity.java
@@ -3,8 +3,11 @@ package com.example.apkadlacebulakw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
+
+    Button btn_sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
